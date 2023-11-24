c4fb2ccc43400f081385389ebef7e983f4b76fcb
mateus-hanel <hanel.mateus@gmail.com>
2023-11-23 22:40:43 -0300

    Data Insertion Refactored

--- src/MongoConnector.py
+++ src/MongoConnector.py
@@ -1,33 +1,22 @@
 import json
 from pymongo import MongoClient
+from data.MongoInserter import MongoInserter
 
 
 class MongoConnector:
-    def __init__(self, connection_path):
+    def __init__(self, connection_path: str):
         self.connection_path = connection_path
-
-    def connect(self):
         self.client = MongoClient(self.connection_path)
         self.db = self.client["historical_events"]
+        self.db = None
+        self.inserter = MongoInserter(self.client, "historical_events")
 
-    def is_collection_created(self, collection_name):
-        list_of_collections = self.db.list_collection_names()
-        return collection_name in list_of_collections
-
-    def insert_data_from_file(self, json_path):
-        if self.is_collection_created("events"):
-            print("Data already imported")
-            return
-        print("Importing Data...")
-        with open(json_path, encoding='utf8') as f:
-            content = f.read()
-            file_data = json.loads(content)
-            print(type(file_data))
-            self.db["events"].insert_many(file_data)
-        print("Data Imported")
+    def insert_historical_data(self):
+        self.inserter .insert_data_from_file(
+            "data/formatted_historical_events.json", "events")
 
 
 if __name__ == "__main__":
+
     connector = MongoConnector("mongodb://localhost:27017/")
-    connector.connect()
-    connector.insert_data_from_file("data/formatted_historical_events.json")
+    connector.insert_historical_data()
---
+++ src/data/MongoInserter.py
@@ -0,0 +1,24 @@
+import json
+from pymongo import MongoClient
+
+
+class MongoInserter:
+    def __init__(self, client: MongoClient, database: str):
+        self.client = client
+        self.db = self.client[database]
+
+    def is_collection_created(self, collection_name):
+        list_of_collections = self.db.list_collection_names()
+        return collection_name in list_of_collections
+
+    def insert_data_from_file(self, json_path: str, collection_name: str):
+        if self.is_collection_created(collection_name):
+            print("Data already imported")
+            return
+        print("Importing Data...")
+        with open(json_path, encoding='utf8') as f:
+            content = f.read()
+            file_data = json.loads(content)
+            print(type(file_data))
+            self.db[collection_name].insert_many(file_data)
+        print("Data Imported")
